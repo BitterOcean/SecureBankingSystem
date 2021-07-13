fda2ac5d121587a1f5bf19eb724aa8d1df938374
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2021-07-13 22:23:56 +0430

    edit database DDL-script add join_request table

--- Database/BankDDL.sql
+++ Database/BankDDL.sql
@@ -1,7 +1,9 @@
 USE securebankingsystem
 GO
 
-CREATE OR REPLACE TABLE User (
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User (
   username VARCHAR(50) PRIMARY KEY,
   `password` VARCHAR(200) NOT NULL,
   salt VARCHAR(100) NOT NULL,
@@ -9,7 +11,8 @@ CREATE OR REPLACE TABLE User (
 );
 
 
-CREATE OR REPLACE TABLE Account (
+DROP TABLE IF EXISTS Account;
+CREATE TABLE Account (
   account_no INT(10) AUTO_INCREMENT PRIMARY KEY,
   opener_ID VARCHAR(50) NOT NULL,
   `type` VARCHAR(30) NOT NULL
@@ -29,7 +32,8 @@ CREATE OR REPLACE TABLE Account (
 ALTER TABLE Account AUTO_INCREMENT = 1000000000;
 
 
-CREATE OR REPLACE TABLE Account_User (
+DROP TABLE IF EXISTS Account_User;
+CREATE TABLE Account_User (
   account_user_ID INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
   account_no INT(10) NOT NULL,
@@ -41,7 +45,8 @@ CREATE OR REPLACE TABLE Account_User (
 );
 
 
-CREATE OR REPLACE TABLE `Transaction` (
+DROP TABLE IF EXISTS `Transaction`;
+CREATE TABLE `Transaction` (
   transaction_ID INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
   from_account_no INT(10) NOT NULL,
@@ -55,9 +60,24 @@ CREATE OR REPLACE TABLE `Transaction` (
 );
 
 
-CREATE OR REPLACE TABLE Signup_Request_Log (
+DROP TABLE IF EXISTS Join_Request;
+CREATE TABLE Join_Request (
+  join_ID INT AUTO_INCREMENT PRIMARY KEY,
+  applicant_username VARCHAR(50) NOT NULL,
+  desired_account_no INT(10) NOT NULL,
+  `status` VARCHAR(1) NOT NULL DEFAULT '0' CHECK(`status` IN ('0', '1')), -- 0: pending, 1: accept
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (applicant_username) REFERENCES User(username),
+  FOREIGN KEY (desired_account_no) REFERENCES Account(account_no)
+);
+
+
+DROP TABLE IF EXISTS Signup_Request_Log;
+CREATE TABLE Signup_Request_Log (
   signup_log_ID INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
+  `password` VARCHAR(200) NOT NULL,
 	`status` VARCHAR(1) CHECK(`status` IN ('1', '0') ),
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -68,14 +88,26 @@ CREATE OR REPLACE TABLE Login_Request_Log (
   username VARCHAR(50) NOT NULL,
 	`password` VARCHAR(200) NOT NULL,
 	`status` VARCHAR(1) CHECK(`status` in ('1', '0') ),
-  mac VARCHAR(20) NOT NULL,
 	ip VARCHAR(20) NOT NULL,
 	port VARCHAR(20) NOT NULL,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
-CREATE OR REPLACE TABLE Ban_Users(
+DROP TABLE IF EXISTS Joint_Request_Log;
+CREATE TABLE Joint_Request_Log (
+  join_log_ID INT AUTO_INCREMENT PRIMARY KEY,
+  applicant_username VARCHAR(50) NOT NULL,
+  desired_account_no INT(10) NOT NULL,
+  conf_lable VARCHAR(1) NULL CHECK(conf_lable IN ('1','2','3','4') ),
+	integrity_lable VARCHAR(1) NULL CHECK(integrity_lable IN ('1','2','3','4')),
+  `type` VARCHAR(8) CHECK(`type` IN ('join', 'accept')),
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS Ban_Users;
+CREATE TABLE Ban_Users(
 	username VARCHAR(50) NOT NULL,
 	ban_times INT NOT NULL DEFAULT 0,
 	started_at DATETIME,
