9f75b7b9381504916a4a47d9dd04d65e953963b2
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2021-07-13 14:50:52 +0430

    edit database DML-script 1st part

--- Database/BankDML.sql
+++ Database/BankDML.sql
@@ -1,133 +1,192 @@
 USE securebankingsystem
 GO
 
+----------------------------------------
+------------- Procedures ---------------
+----------------------------------------
+DROP PROCEDURE IF EXISTS check_user;
+DELIMITER $$
+CREATE PROCEDURE check_user(
+	IN _username VARCHAR(50),
+  OUT _status INT
+)
+BEGIN
+	DECLARE numOfUsers DECIMAL DEFAULT 0;
+  SELECT COUNT(*)
+  INTO numOfUsers
+  FROM User
+  WHERE username = _username;
 
-DROP FUNCTION IF EXISTS is_ban;
-DELIMITER //
+  IF numOfUsers > 0 THEN
+      SET _status = 1;
+  ELSE
+      SET _status = 0;
+  END IF;
+END$$
 
-CREATE FUNCTION is_ban(_username varchar(255))
-  RETURNS INT
-  LANGUAGE SQL
-  DETERMINISTIC
-  CONTAINS SQL
-  SQL SECURITY DEFINER
-  COMMENT 'returns 0 if the user is banned and 1 otherwise'
-    BEGIN
-      SET @ban_times_var := (SELECT ban_times FROM Ban_Users WHERE username = _username);
-      SET @started_at_var := (SELECT started_at FROM Ban_Users WHERE username = _username);
-      SET @finished_at_var := (SELECT finished_at FROM Ban_Users WHERE username = _username);
-
-      IF (@started_at_var IS NULL OR @ban_times_var = 0) THEN
-        SET @ret = 0;
-      ELSE
-        IF (TIMESTAMPDIFF(MINUTE, @started_at_var, @finished_at_var) < 0) THEN
-          SET @ret = 1;
-        ELSE
-          SET @ret = 0;
-        END IF;
-      END IF;
-
-      RETURN @ret;
-    END
-//
 DELIMITER ;
-GO
 
 
-DROP FUNCTION IF EXISTS get_last_failed_login;
-DELIMITER //
+DROP PROCEDURE IF EXISTS add_user;
+DELIMITER $$
+CREATE PROCEDURE add_user(
+	IN _username VARCHAR(50),
+  IN _password VARCHAR(200),
+  IN _salt VARCHAR(100)
+)
+BEGIN
+	START TRANSACTION;
+	INSERT INTO user (username, `password`, salt)
+	  VALUES (_username, _password, _salt);
+	COMMIT;
+END$$
 
-CREATE FUNCTION get_last_failed_login(_username VARCHAR(255))
-  RETURNS INT
-  LANGUAGE SQL
-  DETERMINISTIC
-  CONTAINS SQL
-  SQL SECURITY DEFINER
-  COMMENT 'returns the number of failed logins from the last successful login'
-    BEGIN
-      SET @one_day_befor_today = (SELECT ADDDATE(CURDATE(), INTERVAL -1 DAY));
-
-      SET @last_success_login = (
-        SELECT created_at
-        FROM Login_Request_Log
-        WHERE username = _username
-          AND `status` = '1'
-      );
-
-      SET @max_date = (
-        SELECT CASE
-            WHEN @last_success_login > @one_day_befor_today THEN @last_success_login
-            ELSE @one_day_befor_today
-          END
-      );
-
-      SET @ret = (
-        SELECT count(*)
-        FROM Login_Request_Log
-        WHERE username = _username
-          AND `status` = '0'
-          AND created_at > @max_date
-      );
-
-      RETURN @ret;
-    END
-//
 DELIMITER ;
-GO
 
 
-DROP FUNCTION IF EXISTS check_password;
-DELIMITER //
+DROP PROCEDURE IF EXISTS get_password;
+DELIMITER $$
+CREATE PROCEDURE get_password(
+	IN _username VARCHAR(50),
+  OUT _password VARCHAR(200)
+)
+BEGIN
+    SELECT `password`
+    INTO _password
+    FROM user
+    WHERE username = _username;
+END$$
 
-CREATE FUNCTION check_password(_username VARCHAR(255), _password VARCHAR(500))
-  RETURNS INT
-  LANGUAGE SQL
-  DETERMINISTIC
-  CONTAINS SQL
-  SQL SECURITY DEFINER
-  COMMENT 'returns 1 if the password is correct and 0 otherwise'
-    BEGIN
-      SET @ret = 0;
-      SET @pass = (
-        SELECT `password`
-        FROM User
-        WHERE username = _username
-      );
-
-      IF @pass = _password THEN
-        SET @ret = 1;
-      END IF;
-
-      RETURN @ret;
-    END
-//
 DELIMITER ;
-GO
 
 
+DROP PROCEDURE IF EXISTS check_ban;
+DELIMITER $$
+CREATE PROCEDURE check_ban(
+	IN _username VARCHAR(50),
+  OUT _status INT
+)
+BEGIN
+	DECLARE ban DECIMAL DEFAULT 0;
+  SELECT COUNT(*)
+  INTO ban
+  FROM ban_users
+  WHERE username = _username
+    AND ban_times <> 0
+    AND CURRENT_TIMESTAMP < finished_at;
+  IF ban > 0 THEN
+      SET _status = 1;
+  ELSE
+      SET _status = 0;
+  END IF;
+END$$
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS deposit;
+DELIMITER $$
+CREATE PROCEDURE deposit(
+  IN _username VARCHAR(50),
+	IN _from_account_no INT(10),
+  IN _to_account_no INT(10),
+  IN _amount DECIMAL(7, 4)
+)
+BEGIN
+	START TRANSACTION;
+	INSERT INTO `Transaction` (
+    username,
+    from_account_no,
+    to_account_no,
+    amount,
+    `status`
+    )
+	  VALUES (
+      _username,
+      _from_account_no,
+      _to_account_no,
+      _amount,
+      '1'
+    );
+	COMMIT;
+END$$
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS withdraw;
+DELIMITER $$
+CREATE PROCEDURE withdraw(
+  IN _username VARCHAR(50),
+	IN _from_account_no INT(10),
+  IN _to_account_no INT(10),
+  IN _amount DECIMAL(7, 4)
+)
+BEGIN
+	START TRANSACTION;
+	INSERT INTO `Transaction` (
+    username,
+    from_account_no,
+    to_account_no,
+    amount,
+    `status`
+    )
+	  VALUES (
+      _username,
+      _from_account_no,
+      _to_account_no,
+      (0 - _amount),
+      '1'
+    );
+	COMMIT;
+END$$
+
+DELIMITER ;
+
+----------------------------------------
+------------- Triggers -----------------
+----------------------------------------
 DROP TRIGGER IF EXISTS auto_updated_at;
-DELIMITER //
-
+DELIMITER $$
 CREATE TRIGGER auto_updated_at
 AFTER UPDATE
 ON Account
 FOR EACH ROW
   UPDATE Account
   SET NEW.updated_at = CURRENT_TIMESTAMP
-//
+$$
 DELIMITER ;
 GO
 
 
 DROP TRIGGER IF EXISTS auto_insert_ban_user;
-DELIMITER //
-
+DELIMITER $$
 CREATE TRIGGER auto_insert_ban_user
 AFTER INSERT
 ON User
 FOR EACH ROW
   INSERT INTO Ban_Users ( username, ban_times, started_at, finished_at )
     VALUES(NEW.username, 0, NULL, NULL)
-//
+$$
+DELIMITER ;
+GO
+
+
+DROP TRIGGER IF EXISTS auto_update_account_balance_after_transaction;
+DELIMITER $$
+CREATE TRIGGER auto_update_account_balance_after_transaction
+AFTER INSERT
+ON `Transaction`
+FOR EACH ROW
+BEGIN
+  -- update destination account balance
+  UPDATE Account
+  SET amount = amount + NEW.amount
+  WHERE account_no = NEW.to_account_no;
+  -- update origin account balance
+  UPDATE Account
+  SET amount = amount - NEW.amount
+  WHERE account_no = NEW.from_account_no;
+END$$
 DELIMITER ;
 GO
