f3a011686baeedcf7e796993fb28d1b4e432a016
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2021-07-15 01:41:45 +0430

    edit client: add withdraw and deposit

--- Server.py
+++ Server.py
@@ -1,39 +1,41 @@
-import os
-import json
+import _thread
 import base64
+import hashlib
+import json
+import os
+import secrets
 import socket
 import string
-import _thread
-import secrets
-import hashlib
-import mysql.connector
-from dotenv import load_dotenv
-from Crypto import Random
 from datetime import datetime
+
+import mysql.connector
+from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
-from password_strength import PasswordPolicy
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from dotenv import load_dotenv
+from password_strength import PasswordPolicy
 
 count = 0
 load_dotenv()
 policy = PasswordPolicy.from_names(
-    length = 10,  # min length: 8
-    uppercase = 1,  # need min. 2 uppercase letters
-    numbers = 1,  # need min. 2 digits
-    special = 1,  # need min. 2 special characters
-    nonletters = 1,  # need min. 2 non-letter characters (digits, specials, anything)
-    entropybits = 30,  # need a password that has minimum 30 entropy bits (the power of its alphabet)
+    length=10,  # min length: 8
+    uppercase=1,  # need min. 2 uppercase letters
+    numbers=1,  # need min. 2 digits
+    special=1,  # need min. 2 special characters
+    nonletters=1,  # need min. 2 non-letter characters (digits, specials, anything)
+    entropybits=30,  # need a password that has minimum 30 entropy bits (the power of its alphabet)
 )
 connection = mysql.connector.connect(
-    host = 'localhost',
-    database = 'securebankingsystem',
-    user = os.getenv('DATABASE_USERNAME'),
-    password = os.getenv('DATABASE_PASSWORD')
+    host='localhost',
+    database='securebankingsystem',
+    user=os.getenv('DATABASE_USERNAME'),
+    password=os.getenv('DATABASE_PASSWORD')
 )
+
 
 def client_service(client):
     global count
@@ -48,7 +50,8 @@ def client_service(client):
         except:
             if not data:
                 c_IP, c_port = client.getpeername()
-                print('Client with ip = {} and port = {} has been disconnected at time {}.'.format(c_IP, c_port, str(datetime.now())))
+                print('Client with ip = {} and port = {} has been disconnected at time {}.'
+                      .format(c_IP, c_port, str(datetime.now())))
                 client.close()
                 count = count - 1
                 return 1
@@ -60,6 +63,12 @@ def client_service(client):
         if login:
             if command[0] == "Create" and len(command) == 5:
                 print()
+
+            elif command[0] == "Deposit" and len(command) == 4:
+                pass
+
+            elif command[0] == "Withdraw" and len(command) == 3:
+                pass
 
             elif command[0] == "Exit" and len(command) == 1:
                 break
@@ -103,14 +112,14 @@ def client_service(client):
     client.close()
     count = count - 1
 
+
 def check_username(username):
     cursor = connection.cursor()
-    args = []
-    args.append(username)
-    args.append(0)
+    args = [username, 0]
     result_args = cursor.callproc('check_user', args)
     cursor.close()
     return result_args[1]
+
 
 def is_password_strong(username, password):
     if password.find(username) != -1:
@@ -128,24 +137,20 @@ def is_password_strong(username, password):
 
     return '1'
 
+
 def add_user(username, password):
     cursor = connection.cursor()
     salt = ''.join(secrets.choice(string.ascii_letters) for _ in range(25))
     hash_password = hashlib.sha256((password + salt).encode('utf-8')).hexdigest()
     hash_password = str(hash_password)
-    args = []
-    args.append(username)
-    args.append(hash_password)
-    args.append(salt)
+    args = [username, hash_password, salt]
     cursor.callproc('add_user', args)
     cursor.close()
 
+
 def check_password(username, password):
     cursor = connection.cursor()
-    args = []
-    args.append(username)
-    args.append(0)
-    args.append(0)
+    args = [username, 0, 0]
     result_args = cursor.callproc('get_password_salt', args)
     salt = str(result_args[2])
     hash_password = hashlib.sha256((password + salt).encode('utf-8')).hexdigest()
@@ -156,37 +161,42 @@ def check_password(username, password):
     cursor.close()
     return 0
 
+
 def key_exchange(client):
     backend = default_backend()
     client_recv = client.recv(1024)
     client_public_key = serialization.load_pem_public_key(client_recv, backend)
     server_private_key = ec.generate_private_key(ec.SECP256R1(), backend)
     server_public_key = server_private_key.public_key()
-    client.send(server_public_key.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo))
+    client.send(
+        server_public_key.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo))
     shared_data = server_private_key.exchange(ec.ECDH(), client_public_key)
     secret_key = HKDF(hashes.SHA256(), 32, None, b'Key Exchange', backend).derive(shared_data)
     session_key = secret_key[-16:]
     return session_key
 
+
 def encrypt(plaintext, key):
     nonce1 = Random.get_random_bytes(8)
     countf = Counter.new(64, nonce1)
-    cipher = AES.new(key, AES.MODE_CTR, counter = countf)
+    cipher = AES.new(key, AES.MODE_CTR, counter=countf)
     ciphertext_bytes = cipher.encrypt(plaintext.encode('utf-8'))
     nonce = base64.b64encode(nonce1).decode('utf-8')
     ciphertext = base64.b64encode(ciphertext_bytes).decode('utf-8')
     result = json.dumps({'nonce': nonce, 'ciphertext': ciphertext})
     return result
 
+
 def decrypt(ciphertext, key):
     b64 = json.loads(ciphertext)
     nonce = base64.b64decode(b64['nonce'].encode('utf-8'))
     plaintext = base64.b64decode(b64['ciphertext'])
     countf = Counter.new(64, nonce)
-    cipher = AES.new(key, AES.MODE_CTR, counter = countf)
+    cipher = AES.new(key, AES.MODE_CTR, counter=countf)
     plaintext = cipher.decrypt(plaintext)
     result = plaintext.decode('utf-8')
     return result
+
 
 if __name__ == '__main__':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
