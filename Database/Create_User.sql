1dcf10b55c4a28309284d6c3b7b7215f669a3efd
RezaBlaugrana <mazrouee78@gmail.com>
2021-07-15 12:24:06 -0600

    edit create user

--- Database/Create_User.sql
+++ Database/Create_User.sql
@@ -1,11 +1,3 @@
 CREATE USER 'pynative'@'localhost' IDENTIFIED BY 'pynative@#29';
 GRANT ALL PRIVILEGES ON * . * TO 'pynative'@'localhost';
 FLUSH PRIVILEGES;
-
-
-
-
-SELECT *
-FROM login_request_log
-ORDER BY created_at DESC
-LIMIT 5
