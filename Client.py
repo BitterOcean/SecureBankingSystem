59e5d0976f91968561b5d6a6fbc3422dd8bad6ce
Maryam Saeedmehr <6520696-BitterOcean@users.noreply.gitlab.com>
2021-07-15 22:27:10 +0430

    update Server and Client: add create account command

--- Client.py
+++ Client.py
@@ -11,66 +11,22 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 
-def Signup(command, client, key):
-    command = encrypt(command, key)
-    client.send(command.encode('utf-8'))
-
-    replay = client.recv(1024)
-    replay = replay.decode('utf-8')
-    replay = decrypt(replay, key)
-
-    replay = replay.split()
-    if replay[0] == "ok":
-        print("Signup successfully.")
-        return 1  # Signup successfully
-    elif replay[0] == "E0":
-        print("ERROR: This username already exists. Please select another one.")
-        return 0  # Signup failed
-    elif replay[0] == "E1":
-        print("ERROR: Password is not strong enough. {}".format(replay[3:]))
-        return 0  # Signup failed
-
-
-def Login(command, client, key):
-    command = encrypt(command, key)
-    client.send(command.encode('utf-8'))
-
-    replay = client.recv(1024)
-    replay = replay.decode('utf-8')
-    replay = decrypt(replay, key)
-
-    replay = replay.split()
-    if replay[0] == "ok":
-        print("Logged in successfully.")
-        return 1  # Login successfully
-    elif replay[0] == "E0":
-        print("ERROR: Username do not match. Please try again")
-        return 0  # Login failed
-    elif replay[0] == "E1":
-        print("ERROR: Password do not match. Please try again")
-        return 0  # Login failed
-    '''elif replay[0] == "ban":
-        print("ERROR: Your Account is ban for {} minute. try again later".format(replay[2]))
-        return 0  # Login failed'''
-
-
 def Create(command, cmd, client, key):
     conf_label = ["TopSecret", "Secret", "Confidential", "Unclassified"]
     integrity_label = ["VeryTrusted", "Trusted", "SlightlyTrusted", "Untrusted"]
-    account_type = ["Short-term saving account", "Long-term saving account", "Current account",
-                    "Gharz al-Hasna saving account"]
 
-    if ~(0 <= int(cmd[1]) <= 3):
-        print()
+    if not (0 <= int(cmd[1]) <= 3):
+        print("ERROR: Account type is out of range!!! Please try again")
         return 0
     if float(cmd[2]) < 0:
-        print()
+        print("ERROR: Amount of money received is negative. Please try again")
         return 0
     if cmd[3] not in conf_label:
-        print()
+        print("ERROR: Confidentiality label is not defined, Please try again and enter an allowable Confidentiality "
+              "label")
         return 0
     if cmd[4] not in integrity_label:
-        print()
+        print("ERROR: Integrity label is not defined. Please try again and enter an allowable integrity label")
         return 0
 
     command = encrypt(command, key)
@@ -79,49 +35,19 @@ def Create(command, cmd, client, key):
     replay = client.recv(1024)
     replay = replay.decode('utf-8')
     replay = decrypt(replay, key)
-    print()
+    print("Your account has been created successfully!!, Your account number is: {}"
+          .format(replay))
 
 
-def Withdraw(command, cmd, client, key):
-    """
-    :param command: withdraw [account_no] [amount]
-    :param cmd: list('withdraw', account_no, amount)
-    :param client: socket of current client to server
-    :param key: session key
-    :return: status code (1: successful, 0: failure)
-    """
-    if len(cmd[1]) != 10:
-        print("ERROR: Invalid account number. [Length(10)]")
-        return 0
-    if not (cmd[1].isnumeric()):
-        print("ERROR: Invalid account number. [Digits Only]")
-        return 0
-    if len(cmd[2]) > 11:
-        print("ERROR: Out of range amount. [Length(11)]")
-        return 0
-    if float(cmd[2]) < 0:
-        print("ERROR: Invalid amount. [Positive Number Only]")
-        return 0
-
-    command = encrypt(command, key)
-    client.send(command.encode('utf-8'))
-
-    replay = client.recv(1024)
-    replay = replay.decode('utf-8')
-    replay = decrypt(replay, key)
-    replay = replay.split()
-    if replay[0] == "ok":
-        print("Withdrawal was successful.")
-        return 1  # Withdrawal was successful
-    elif replay[0] == "E0":
-        print("ERROR: There is no account with the given account_no.")
-        return 0  # Withdrawal was failed
-    elif replay[0] == "E1":
-        print("ERROR: Access denied. You are not permitted to withdraw from this account")
-        return 0  # Withdrawal was failed
-    elif replay[0] == "E2":
-        print("ERROR: Your account balance is not enough.")
-        return 0  # Withdrawal was failed
+def decrypt(ciphertext, key):
+    b64 = json.loads(ciphertext)
+    nonce = base64.b64decode(b64['nonce'].encode('utf-8'))
+    plaintext = base64.b64decode(b64['ciphertext'])
+    countf = Counter.new(64, nonce)
+    cipher = AES.new(key, AES.MODE_CTR, counter=countf)
+    plaintext = cipher.decrypt(plaintext)
+    result = plaintext.decode('utf-8')
+    return result
 
 
 def Deposit(command, cmd, client, key):
@@ -175,21 +101,6 @@ def Deposit(command, cmd, client, key):
         return 0  # Deposit was failed
 
 
-def key_exchange(client):
-    backend = default_backend()
-    client_private_key = ec.generate_private_key(ec.SECP256R1(), backend)
-    client_public_key = client_private_key.public_key()
-    client.send(
-        client_public_key.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo))
-    server_recv = client.recv(1024)
-    server_public_key = serialization.load_pem_public_key(server_recv, backend)
-    shared_data = client_private_key.exchange(ec.ECDH(), server_public_key)
-    secret_key = HKDF(hashes.SHA256(), 32, None, b'Key Exchange', backend).derive(shared_data)
-    session_key = secret_key[-16:]
-    print("Key exchanged successfully.\n")
-    return session_key
-
-
 def encrypt(plaintext, key):
     nonce1 = Random.get_random_bytes(8)
     countf = Counter.new(64, nonce1)
@@ -201,15 +112,105 @@ def encrypt(plaintext, key):
     return result
 
 
-def decrypt(ciphertext, key):
-    b64 = json.loads(ciphertext)
-    nonce = base64.b64decode(b64['nonce'].encode('utf-8'))
-    plaintext = base64.b64decode(b64['ciphertext'])
-    countf = Counter.new(64, nonce)
-    cipher = AES.new(key, AES.MODE_CTR, counter=countf)
-    plaintext = cipher.decrypt(plaintext)
-    result = plaintext.decode('utf-8')
-    return result
+def key_exchange(client):
+    backend = default_backend()
+    client_private_key = ec.generate_private_key(ec.SECP256R1(), backend)
+    client_public_key = client_private_key.public_key()
+    client.send(client_public_key.public_bytes(serialization.Encoding.PEM,
+                                               serialization.PublicFormat.SubjectPublicKeyInfo))
+    server_recv = client.recv(1024)
+    server_public_key = serialization.load_pem_public_key(server_recv, backend)
+    shared_data = client_private_key.exchange(ec.ECDH(), server_public_key)
+    secret_key = HKDF(hashes.SHA256(), 32, None, b'Key Exchange', backend).derive(shared_data)
+    session_key = secret_key[-16:]
+    print("Key exchanged successfully.\n")
+    return session_key
+
+
+def Login(command, client, key):
+    command = encrypt(command, key)
+    client.send(command.encode('utf-8'))
+
+    replay = client.recv(1024)
+    replay = replay.decode('utf-8')
+    replay = decrypt(replay, key)
+
+    replay = replay.split()
+    if replay[0] == "ok":
+        print("Logged in successfully.")
+        return 1  # Login successfully
+    elif replay[0] == "E0":
+        print("ERROR: Username do not match. Please try again")
+        return 0  # Login failed
+    elif replay[0] == "E1":
+        print("ERROR: Password do not match. Please try again")
+        return 0  # Login failed
+    elif replay[0] == "ban":
+        print("ERROR: Your Account is ban for {} seconds. try again later"
+              .format(replay[1]))
+        return 0  # Login failed
+
+
+def Signup(command, client, key):
+    command = encrypt(command, key)
+    client.send(command.encode('utf-8'))
+
+    replay = client.recv(1024)
+    replay = replay.decode('utf-8')
+    replay = decrypt(replay, key)
+
+    replay = replay.split()
+    if replay[0] == "ok":
+        print("Signup successfully.")
+        return 1  # Signup successfully
+    elif replay[0] == "E0":
+        print("ERROR: This username already exists. Please select another one.")
+        return 0  # Signup failed
+    elif replay[0] == "E1":
+        print("ERROR: Password is not strong enough. {}".format(replay[3:]))
+        return 0  # Signup failed
+
+
+def Withdraw(command, cmd, client, key):
+    """
+    :param command: withdraw [account_no] [amount]
+    :param cmd: list('withdraw', account_no, amount)
+    :param client: socket of current client to server
+    :param key: session key
+    :return: status code (1: successful, 0: failure)
+    """
+    if len(cmd[1]) != 10:
+        print("ERROR: Invalid account number. [Length(10)]")
+        return 0
+    if not (cmd[1].isnumeric()):
+        print("ERROR: Invalid account number. [Digits Only]")
+        return 0
+    if len(cmd[2]) > 11:
+        print("ERROR: Out of range amount. [Length(11)]")
+        return 0
+    if float(cmd[2]) < 0:
+        print("ERROR: Invalid amount. [Positive Number Only]")
+        return 0
+
+    command = encrypt(command, key)
+    client.send(command.encode('utf-8'))
+
+    replay = client.recv(1024)
+    replay = replay.decode('utf-8')
+    replay = decrypt(replay, key)
+    replay = replay.split()
+    if replay[0] == "ok":
+        print("Withdrawal was successful.")
+        return 1  # Withdrawal was successful
+    elif replay[0] == "E0":
+        print("ERROR: There is no account with the given account_no.")
+        return 0  # Withdrawal was failed
+    elif replay[0] == "E1":
+        print("ERROR: Access denied. You are not permitted to withdraw from this account")
+        return 0  # Withdrawal was failed
+    elif replay[0] == "E2":
+        print("ERROR: Your account balance is not enough.")
+        return 0  # Withdrawal was failed
 
 
 if __name__ == '__main__':
@@ -271,11 +272,16 @@ if __name__ == '__main__':
                     print(help_message2)
 
                 elif cmd[0] == "Exit" and len(cmd) == 1:
-
+                    command = encrypt(command, session_key)
+                    client.send(command.encode('utf-8'))
+                    replay = client.recv(1024)
+                    replay = replay.decode('utf-8')
+                    replay = decrypt(replay, session_key)
+                    print(replay)
                     break
 
                 else:
-                    print()
+                    print("Command is wrong!!!")
 
             else:
                 if cmd[0] == "Signup" and len(cmd) == 3:
@@ -291,10 +297,15 @@ if __name__ == '__main__':
                     print(help_message1)
 
                 elif cmd[0] == "Exit" and len(cmd) == 1:
-
+                    command = encrypt(command, session_key)
+                    client.send(command.encode('utf-8'))
+                    replay = client.recv(1024)
+                    replay = replay.decode('utf-8')
+                    replay = decrypt(replay, session_key)
+                    print(replay)
                     break
 
                 else:
-                    print()
+                    print("Command is wrong!!!")
 
         client.close()
